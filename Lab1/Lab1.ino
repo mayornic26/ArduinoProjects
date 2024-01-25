533633d3fb87f6bb0ab8687f7b6caca6bf90e37e
mayornic26 <114428885+mayornic26@users.noreply.github.com>
2024-01-25 11:00:40 -0500

    Update Lab1.ino

--- Lab1/Lab1.ino
+++ Lab1/Lab1.ino
@@ -1,1 +1,20 @@
+void setup() {
+  pinMode(13, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(11, OUTPUT);
+}
 
+void loop() {
+  //red
+  digitalWrite(13, HIGH);
+  delay(5000);
+  digitalWrite(13, LOW);
+  //green
+  digitalWrite(12, HIGH);
+  delay(5000);
+  digitalWrite(12, LOW);
+  //yellow
+  digitalWrite(11, HIGH);
+  delay(2000);
+  digitalWrite(11, LOW);
+}
