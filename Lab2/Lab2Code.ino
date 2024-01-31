942412cdbf447974bde8e6a2304bcaf6a37f94fd
mayornic26 <114428885+mayornic26@users.noreply.github.com>
2024-01-31 10:40:14 -0500

    Update Lab2Code.ino

--- Lab2/Lab2Code.ino
+++ Lab2/Lab2Code.ino
@@ -1,15 +1,18 @@
-  int potPosition;
+//Nico Mayoral
+//1/31/23
+//Uses Potentionmeter to change the speed in which the LED is flashing at
+int potPosition;
 
  void setup() {
-  pinMode(13, OUTPUT);
-  Serial.begin(9600);
+    pinMode(13, OUTPUT);
+    Serial.begin(9600);
 }
 
   void loop() {
-      potPosition = analogRead(0);
-  Serial.println(potPosition);
-  digitalWrite(13, HIGH);
-  delay(potPosition);
-  digitalWrite(13, LOW);
-  delay(potPosition);
+    potPosition = analogRead(0);
+    Serial.println(potPosition);
+    digitalWrite(13, HIGH);
+    delay(potPosition);
+    digitalWrite(13, LOW);
+    delay(potPosition);
 }
