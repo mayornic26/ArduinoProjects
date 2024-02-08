b5a3204fe73739995b7c0e443bc656532bcda97e
mayornic26 <114428885+mayornic26@users.noreply.github.com>
2024-02-08 10:03:03 -0500

    Update Lab5.ino

--- Lab5/Lab5.ino
+++ Lab5/Lab5.ino
@@ -1,1 +1,32 @@
+const int buttonPin1 = 2;
+const int buttonPin2 = 3;
+const int ledPin = 13;
 
+
+int buttonState1= 0;
+int buttonState2 = 0;
+
+void setup()
+{
+  pinMode(ledPin, OUTPUT);
+  pinMode(buttonPin1, INPUT);
+  pinMode(buttonPin2, INPUT);
+ 
+}
+
+void loop()
+{
+  buttonState1 = digitalRead(buttonPin1);
+  buttonState2= digitalRead(buttonPin2);
+                            
+    if (buttonState1 == LOW && buttonState2 == HIGH ||  buttonState1 == HIGH && buttonState2 == LOW) {
+      digitalWrite(ledPin, LOW);
+    }else if (buttonState1 == LOW && buttonState2 == LOW){
+      digitalWrite(ledPin, HIGH);
+    } else  {
+      digitalWrite(ledPin, HIGH);
+      delay(1000);
+      digitalWrite(ledPin, LOW);
+      delay(1000);
+    }
+}
